7310a9df12f35b717e9ec071925e6570b4bcfc25
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-07-18 23:03:29 +0530

    [Rajesh] Added : Master Branch is added

---
+++ CabInvoiceGenerator/CabInvoiceGenerator/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace CabInvoiceGenerator
+{
+    class Program
+    {
+        public static void Main(string [] args)
+        {
+            Console.WriteLine("\t\t\t\tWelcome to Cab Invoice Generator Problem\t\t\t\t");
+        }
+    }
+}
